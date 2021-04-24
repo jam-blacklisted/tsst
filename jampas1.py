ff9b599f6ff07baa7f4e9de400a5f86792fcc6c6
jam-blacklisted <82037992+jam-blacklisted@users.noreply.github.com>
2021-04-23 17:06:32 -0700

    Update jampas1.py

--- jampas1.py
+++ jampas1.py
@@ -451,15 +451,15 @@ def a_s():
         os.system('clear')
         print logo
         p1 = raw_input(' \033[1;92m[1]Name + digit: ')
-		try:
-	                uidlist = raw_input('[+] File Name: ')
-			        print " ----------------------------------------------- "
-	                for line in open(uidlist ,'r').readlines():
-	                    id.append(line.strip())
+        try:
+	        uidlist = raw_input('[+] File Name: ')
+	        print " ----------------------------------------------- "
+	        for line in open(uidlist ,'r').readlines():
+	            id.append(line.strip())
 		except (KeyError , IOError):
 	            print "    [!] File Not Found."
 	            raw_input('Press Enter To Back. ')
-		        auto_crack()
+		    auto_crack()
         
     elif a_s == '0':
         menu()
@@ -639,15 +639,15 @@ def c_s():
         os.system('clear')
         print logo
         p1 = raw_input(' \033[1;92m[1]Name + digit: ')
-		try:
-	                uidlist = raw_input('[+] File Name: ')
-			        print " ----------------------------------------------- "
-	                for line in open(uidlist ,'r').readlines():
-	                    id.append(line.strip())
+	try:
+	        uidlist = raw_input('[+] File Name: ')
+		print " ----------------------------------------------- "
+	        for line in open(uidlist ,'r').readlines():
+	            id.append(line.strip())
 		except (KeyError , IOError):
 	            print "    [!] File Not Found."
 	            raw_input('Press Enter To Back. ')
-		        auto_crack()
+		    auto_crack()
         
     elif a_s == '0':
         menu()
